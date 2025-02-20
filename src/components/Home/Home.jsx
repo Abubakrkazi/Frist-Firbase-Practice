0af847eae051d3c93901c89c6eb95de413a20709
Abubakr kazi <kaziabubakr87@gmail.com>
2025-02-21 01:04:04 +0600

    first commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="text-4xl"> Welcome For Practice  on Fribase!!!!</h2>
+
+        </div>
+    );
+};
+
+export default Home;
